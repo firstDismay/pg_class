2b9d43135f118bde8abe6e389253c31866bffd83
Dismay <firstdismay@gmail.com>
2022-01-03 04:07:55 +0500

    Контрольная точка

--- pg_class/Properties/AssemblyInfo.cs
+++ pg_class/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.21.12.292")]
-[assembly: AssemblyFileVersion("4.21.12.292")]
+[assembly: AssemblyVersion("4.21.12.302")]
+[assembly: AssemblyFileVersion("4.21.12.302")]
 
 
