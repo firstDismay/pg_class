1e8d0ef0c9f1bb071db2c2f8112610948355bf38
Dmitry Ivanov <firstdismay@gmail.com>
2022-05-21 21:48:59 +0500

    Переработаны функции управления паролями пользователей

---
+++ pg_class/manager/man_role/user/man_user_pwd.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+using pg_class.pg_commands;
+using pg_class.pg_exceptions;
+using pg_class.pg_classes;
+
+namespace pg_class
+{
+    public partial class manager
+    {
+        /// <summary>
+        /// Метод сбрасывает пароль указанного пользователя доступен для администратора
+        /// </summary>
+        public user user_pwd_reset(String login, String newpwd)
+        {
+            user user = null;
+            Int32 error;
+            String desc_error;
+            NpgsqlCommandKey cmdk;
+            //**********
+             
+            //=======================
+            cmdk = CommandByKey("user_pwd_reset");
+
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+            //=======================
+
+            cmdk.Parameters["ilogin"].Value = login;
+            cmdk.Parameters["inewpwd"].Value = newpwd;
+
+            //Начало транзакции
+            cmdk.ExecuteNonQuery();
+            
+            error = Convert.ToInt32(cmdk.Parameters["outresult"].Value);
+            desc_error = Convert.ToString(cmdk.Parameters["outdesc"].Value);
+            //SetLastTimeUsing();
+            //=======================
+            switch (error)
+            {
+                case 0:
+                    user = user_by_login(login);
+                    break;
+                default:
+                    //Вызов события журнала
+                    JournalEventArgs me = new JournalEventArgs(-1, eEntity.user, error, desc_error, eAction.Update, eJournalMessageType.error);
+                    JournalMessageOnReceived(me);
+                    throw new PgDataException(error, desc_error);
+            }
+            //Генерируем событие изменения концепции
+            UserChangeEventArgs e = new UserChangeEventArgs(user, eAction.Update);
+            UserOnChange(e);
+            //Возвращаем Объект
+            return user;
+        }
+
+        //-=ACCESS=-***********************************************************************************
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean user_pwd_reset(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+            //=======================
+            //=======================
+            cmdk = CommandByKey("user_pwd_reset");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Метод меняет пароль пользователя доступен для пользователей
+        /// </summary>
+        public user user_pwd_set(String login, String oldpwd, String newpwd)
+        {
+            user user = null;
+            Int32 error;
+            String desc_error;
+            NpgsqlCommandKey cmdk;
+            //**********
+
+            //=======================
+            cmdk = CommandByKey("user_pwd_set");
+
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+            //=======================
+
+            cmdk.Parameters["ilogin"].Value = login;
+            cmdk.Parameters["ioldpwd"].Value = oldpwd;
+            cmdk.Parameters["inewpwd"].Value = newpwd;
+
+            //Начало транзакции
+            cmdk.ExecuteNonQuery();
+
+            error = Convert.ToInt32(cmdk.Parameters["outresult"].Value);
+            desc_error = Convert.ToString(cmdk.Parameters["outdesc"].Value);
+            //SetLastTimeUsing();
+            //=======================
+            switch (error)
+            {
+                case 0:
+                    user = user_by_login(login);
+                    break;
+                default:
+                    //Вызов события журнала
+                    JournalEventArgs me = new JournalEventArgs(-1, eEntity.user, error, desc_error, eAction.Update, eJournalMessageType.error);
+                    JournalMessageOnReceived(me);
+                    throw new PgDataException(error, desc_error);
+            }
+            //Генерируем событие изменения концепции
+            UserChangeEventArgs e = new UserChangeEventArgs(user, eAction.Update);
+            UserOnChange(e);
+            //Возвращаем Объект
+            return user;
+        }
+
+        //-=ACCESS=-***********************************************************************************
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean user_pwd_set(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+            //=======================
+            //=======================
+            cmdk = CommandByKey("user_pwd_set");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Метод проверяет пароль пользователя
+        /// </summary>
+        public user user_pwd_verify(String login, String pwd)
+        {
+            user user = null;
+            Int32 error;
+            String desc_error;
+            NpgsqlCommandKey cmdk;
+            //**********
+
+            //=======================
+            cmdk = CommandByKey("user_pwd_verify");
+
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+            //=======================
+
+            cmdk.Parameters["ilogin"].Value = login;
+            cmdk.Parameters["ipwd"].Value = pwd;
+
+            //Начало транзакции
+            cmdk.ExecuteNonQuery();
+
+            error = Convert.ToInt32(cmdk.Parameters["outresult"].Value);
+            desc_error = Convert.ToString(cmdk.Parameters["outdesc"].Value);
+            //SetLastTimeUsing();
+            //=======================
+            switch (error)
+            {
+                case 0:
+                    user = user_by_login(login);
+                    break;
+                default:
+                    //Вызов события журнала
+                    JournalEventArgs me = new JournalEventArgs(-1, eEntity.user, error, desc_error, eAction.Update, eJournalMessageType.error);
+                    JournalMessageOnReceived(me);
+                    throw new PgDataException(error, desc_error);
+            }
+            //Генерируем событие изменения концепции
+            UserChangeEventArgs e = new UserChangeEventArgs(user, eAction.Update);
+            UserOnChange(e);
+            //Возвращаем Объект
+            return user;
+        }
+
+        //-=ACCESS=-***********************************************************************************
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean user_pwd_verify(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+            //=======================
+            //=======================
+            cmdk = CommandByKey("user_pwd_verify");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+    }
+}
---
+++ pg_class/manager/man_role/user/man_user_role.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+using pg_class.pg_commands;
+using pg_class.pg_exceptions;
+using pg_class.pg_classes;
+
+namespace pg_class
+{
+    public partial class manager
+    {
+        /// <summary>
+        /// Метод назначает роль пользователю
+        /// </summary>
+        public Boolean user_role_user_grant(user user, role_user role)
+        {
+            Boolean Result = false;
+            Int32 error;
+            String desc_error;
+            NpgsqlCommandKey cmdk;
+            //**********
+            //=======================
+            cmdk = CommandByKey("usr_role_user_grant");
+
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+            //=======================
+
+            cmdk.Parameters["ilogin"].Value = user.Login;
+            cmdk.Parameters["irole_user"].Value = role.NameSystem;
+            try
+            {
+                //Выполнение основной команды в контексте транзакции
+                cmdk.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                cmdk.Transaction.Rollback();
+                PG_exception_hadler(ex, cmdk);
+            }
+            
+            error = Convert.ToInt32(cmdk.Parameters["outresult"].Value);
+            desc_error = Convert.ToString(cmdk.Parameters["outdesc"].Value);
+            //SetLastTimeUsing();
+            //=======================
+            switch (error)
+            {
+                case 0:
+                    Result = true;
+                    break;
+                default:
+                    //Вызов события журнала
+                    JournalEventArgs me = new JournalEventArgs(-1, eEntity.user, error, desc_error, eAction.Update, eJournalMessageType.error);
+                    JournalMessageOnReceived(me);
+                    throw new PgDataException(error, desc_error);
+            }
+            //Генерируем событие изменения концепции
+            UserChangeEventArgs e = new UserChangeEventArgs(user, eAction.Update);
+            UserOnChange(e);
+            //Возвращаем Объект
+            return Result;
+        }
+
+        //-=ACCESS=-***********************************************************************************
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean user_role_user_grant(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+            //=======================
+            //=======================
+            cmdk = CommandByKey("usr_role_user_grant");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+        
+        /// <summary>
+        /// Метод исключает пользователя из роли
+        /// </summary>
+        public Boolean user_role_user_revoke(user user, role_user role)
+        {
+            Boolean Result = false;
+            Int32 error;
+            String desc_error;
+            NpgsqlCommandKey cmdk;
+            //**********
+             
+            //=======================
+            cmdk = CommandByKey("usr_role_user_revoke");
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+            //=======================
+
+            cmdk.Parameters["ilogin"].Value = user.Login;
+            cmdk.Parameters["irole_user"].Value = role.NameSystem;
+
+            //Начало транзакции
+            cmdk.ExecuteNonQuery();
+            
+            error = Convert.ToInt32(cmdk.Parameters["outresult"].Value);
+            desc_error = Convert.ToString(cmdk.Parameters["outdesc"].Value);
+            //SetLastTimeUsing();
+            //=======================
+            switch (error)
+            {
+                case 0:
+                    Result = true;
+                    break;
+                default:
+                    //Вызов события журнала
+                    JournalEventArgs me = new JournalEventArgs(-1, eEntity.user, error, desc_error, eAction.Update, eJournalMessageType.error);
+                    JournalMessageOnReceived(me);
+                    throw new PgDataException(error, desc_error);
+            }
+            //Генерируем событие изменения концепции
+            UserChangeEventArgs e = new UserChangeEventArgs(user, eAction.Update);
+            UserOnChange(e);
+            //Возвращаем Объект
+            return Result;
+        }
+
+        //-=ACCESS=-***********************************************************************************
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean user_role_user_revoke(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+            //=======================
+            //=======================
+            cmdk = CommandByKey("usr_role_user_revoke");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+    }
+}
