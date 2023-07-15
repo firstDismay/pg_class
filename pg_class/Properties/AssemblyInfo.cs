02a473bc5de7c5f761b694daa243d07f6d3dd7e5
Dmitry Ivanov <firstdismay@gmail.com>
2023-07-15 21:05:51 +0500

    fix: Поднять версию класса

--- pg_class/Properties/AssemblyInfo.cs
+++ pg_class/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.23.06.128")]
-[assembly: AssemblyFileVersion("4.23.06.128")]
+[assembly: AssemblyVersion("4.23.07.151")]
+[assembly: AssemblyFileVersion("4.23.07.151")]
 
 
