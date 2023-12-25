1cf5dd4f744463fd06fd9d5d1f59bdc4afd09ae9
Dmitry Ivanov <firstdismay@gmail.com>
2023-12-25 14:30:29 +0500

    feat: Добавить функцию экспорта записей лога по массиву идентификаторов записей лога: public Byte[] exp_log_by_id_log_array_to_excel(Int64[] ilog_array)

---
+++ pg_class/manager/man_export/log/man_log_exp_by_id_log_array.cs
@@ -0,0 +1,71 @@
+﻿using pg_class.pg_commands;
+using pg_class.pg_exceptions;
+using System;
+
+namespace pg_class
+{
+    public partial class manager
+    {
+        /// <summary>
+        /// Метод экспорта записей лога по массиву идентификаторов в файл Excel
+        /// </summary>
+        public Byte[] exp_log_by_id_log_array_to_excel(Int64[] ilog_array)
+        {
+            Byte[] Result = null;
+            NpgsqlCommandKey cmdk;
+
+            cmdk = CommandByKey("exp_log_by_id_log_array_to_excel");
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+
+            cmdk.Parameters["ilog_array"].Value = ilog_array;
+            object tmp = cmdk.ExecuteScalar();
+
+            if (tmp != DBNull.Value)
+            {
+                Result = (Byte[])tmp;
+            }
+
+            String command_export = String.Format(@"SELECT bpd.exp_log_by_id_log_array_to_excel({0})", "Array");
+            //Генерируем событие завершения процедуры экспорта
+            ExportCompletedEventArgs e = new ExportCompletedEventArgs(command_export, Result);
+            ExportOnCompleted(e);
+            return Result;
+        }
+
+        //ACCESS
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean exp_log_by_id_log_array_to_excel(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+
+            cmdk = CommandByKey("exp_log_by_id_log_array_to_excel");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+    }
+}
