e8fee8c6bdd76b7842159873eee525e794bce2f3
Dmitry Ivanov <firstdismay@gmail.com>
2024-01-11 21:52:48 +0500

    feat: Добавлена функция для пакетного обновления схемы данных, доступная для пользователем с правами суперюзера

---
+++ pg_class/manager/man_schema/man_schema_upd.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using pg_class.pg_classes;
+using pg_class.pg_commands;
+using pg_class.pg_exceptions;
+using System;
+using System.Data;
+
+namespace pg_class
+{
+    public partial class manager
+    {
+        /// <summary>
+        /// Метод применяет к текущей базе конструктора файл обновления схемы данных
+        /// </summary>
+        public Int32 schema_update(String filename)
+        {
+            if (this.User_current.RolSuper)
+            {
+                string strText = System.IO.File.ReadAllText(filename, System.Text.Encoding.UTF8);
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = Connect_Get().CN;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strText;
+                return cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу обновления схемы данных текущей базы!"));
+            }
+        }
+    }
+}
