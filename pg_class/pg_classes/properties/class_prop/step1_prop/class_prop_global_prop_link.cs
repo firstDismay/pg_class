3cd4351fcdff8dadde46f08fde3d791a21a1698c
Dmitry Ivanov <firstdismay@gmail.com>
2024-02-18 20:01:20 +0500

    refact: Добавить методы управления ссылками на глобальные свойства в свойства классов и шаблонов позиций

---
+++ pg_class/pg_classes/properties/class_prop/step1_prop/class_prop_global_prop_link.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace pg_class.pg_classes
+{
+    /// <summary>
+    /// Модуль взаимодействия с глобальными свойствами
+    /// </summary>
+    public partial class class_prop
+    {
+        /// <summary>
+        /// Метод добавляет ссылку на глобальное свойство
+        /// </summary>
+        public void global_prop_link_include(long Id_global_prop)
+        {
+            Manager.global_prop_link_class_prop_include(Id_global_prop, this.Id_prop_definition);
+        }
+
+        /// <summary>
+        /// Метод удаляет ссылку на глобальное свойство
+        /// </summary>
+        public void global_prop_link_exclude()
+        {
+            Manager.global_prop_link_class_prop_exclude(this.Id_prop_definition);
+        }
+
+        /// <summary>
+        /// Глобальное свойство связанное со свойством класса
+        /// global_prop_by_id_class_prop_definition
+        /// </summary>
+        public global_prop global_prop_get()
+        {
+            return Manager.global_prop_by_id_class_prop_definition(this);
+        }
+
+        /// <summary>
+        /// Метод определяет готовность свойства к линковке с глобальными свойствами
+        /// </summary>
+        public ePropStateForGlobalPropLink State_for_global_prop_link()
+        {
+            return Manager.class_prop_state_for_global_prop_link(this);
+        }
+    }
+}
---
+++ pg_class/pg_classes/properties/pos_temp_prop/Step1_prop/pos_temp_prop_global_prop_link.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace pg_class.pg_classes
+{
+    /// <summary>
+    /// Модуль взаимодействия сущности и ссылки на документ
+    /// </summary>
+    public partial class pos_temp_prop
+    {
+        /// <summary>
+        /// Метод добавляет ссылку на глобальное свойство
+        /// </summary>
+        public void global_prop_link_include(long Id_global_prop)
+        {
+            Manager.global_prop_link_pos_temp_prop_include(Id_global_prop, this.Id);
+        }
+
+        /// <summary>
+        /// Метод удаляет ссылку на глобальное свойство
+        /// </summary>
+        public void global_prop_link_exclude()
+        {
+            Manager.global_prop_link_pos_temp_prop_exclude(this.Id);
+        }
+
+        /// <summary>
+        /// Глобальное свойство связанное со свойством шаблона
+        /// global_prop_by_id_pos_temp_prop
+        /// </summary>
+        public global_prop global_prop_get()
+        {
+            return Manager.global_prop_by_id_pos_temp_prop(this);
+        }
+    }
+}
