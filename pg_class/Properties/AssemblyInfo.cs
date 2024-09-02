00ed62a6c29c17894803f007b35444e0a429a897
Dmitry Ivanov <firstdismay@gmail.com>
2024-09-02 13:29:19 +0500

    fix: Исправить ошибку npgsql.typemapping.globaltypemapper  exception -2146233036

--- pg_class/Properties/AssemblyInfo.cs
+++ pg_class/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.24.07.31")]
-[assembly: AssemblyFileVersion("4.24.07.31")]
+[assembly: AssemblyVersion("4.24.09.02")]
+[assembly: AssemblyFileVersion("4.24.09.02")]
 
 
