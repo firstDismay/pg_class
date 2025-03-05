80de0d4d2803c5ea1992f7e7a34d8d7a2a34ab8a
Dmitry Ivanov <firstdismay@gmail.com>
2025-03-05 10:49:02 +0500

    fix: Исправить логику генерации событий в методе  object_move
    Новая логика обеспечивает коректную генерацию событий при частичном перемещении объекта

---
+++ pg_class/manager/man_object/man_object _move.cs
@@ -0,0 +1,110 @@
+﻿using pg_class.pg_classes;
+using pg_class.pg_commands;
+using pg_class.pg_exceptions;
+using System;
+
+namespace pg_class
+{
+    public partial class manager
+    {
+        /// <summary>
+        /// Метод переносит объект в новое расположение
+        /// </summary>
+        public object_general object_move(Int64 iid_object, Int64 iid_position, Decimal icquantity)
+        {
+            object_general Object_move = null;
+            object_general Object_change = null;
+            object_general Result = null;
+            Int64 id = 0;
+            NpgsqlCommandKey cmdk;
+
+            if (iid_object > 0)
+            {
+                Object_move = object_by_id(iid_object);
+            }
+
+            cmdk = CommandByKey("object_move");
+            if (cmdk != null)
+            {
+                if (!cmdk.Access)
+                {
+                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
+                }
+            }
+            else
+            {
+                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
+            }
+
+            cmdk.Parameters["iid_object"].Value = iid_object;
+            cmdk.Parameters["iid_position"].Value = iid_position;
+            cmdk.Parameters["icquantity"].Value = icquantity;
+            cmdk.ExecuteNonQuery();
+
+            id = Convert.ToInt64(cmdk.Parameters["outid"].Value);
+            if (Object_move != null)
+            {
+                if ((id > 0) && (id != Object_move.Id))
+                {
+                    Object_change = object_by_id(iid_object);
+                    Object_move = object_by_id(id);
+                    Result = Object_change;
+                }
+                else
+                {
+                    Object_move = object_by_id(id);
+                    Result = Object_move;
+                }
+            }
+
+            if (Object_move != null)
+            {
+                //Генерируем событие изменения объекта
+                ObjectChangeEventArgs e = new ObjectChangeEventArgs(Object_move, eAction.Move);
+                ObjectOnChange(e);
+            }
+            //Если есть остаток то генерируем изменение остатка
+            if (Object_change != null)
+            {
+                ObjectChangeEventArgs e3 = new ObjectChangeEventArgs(Object_change, eAction.Update);
+                ObjectOnChange(e3);
+            }
+            //Возвращаем сущность
+            return Result;
+        }
+
+        /// <summary>
+        /// Метод переносит объект в новое расположение
+        /// </summary>
+        public object_general object_move(object_general Object, position Position, Decimal icuantity)
+        {
+            return object_move(Object.Id, Position.Id, icuantity);
+        }
+
+        //ACCESS
+        /// <summary>
+        /// Проверка прав доступа к методу
+        /// </summary>
+        public Boolean object_move(out eAccess Access)
+        {
+            Boolean Result = false;
+            Access = eAccess.NotFound;
+            NpgsqlCommandKey cmdk;
+
+            cmdk = CommandByKey("object_move");
+            if (cmdk != null)
+            {
+                Result = cmdk.Access;
+                if (Result)
+                {
+                    Access = eAccess.Success;
+                }
+                else
+                {
+                    Access = eAccess.NotAvailable;
+                }
+            }
+            return Result;
+        }
+    }
+}
--- pg_class/manager/man_object/man_object _upd.cs
+++ pg_class/manager/man_object/man_object _upd.cs
@@ -14,8 +14,6 @@ namespace pg_class
 
         {
             object_general Object = null;
-            Int32 error;
-            String desc_error;
             NpgsqlCommandKey cmdk;
 
             cmdk = CommandByKey("object_upd");
@@ -68,94 +66,6 @@ namespace pg_class
             NpgsqlCommandKey cmdk;
 
             cmdk = CommandByKey("object_upd");
-            if (cmdk != null)
-            {
-                Result = cmdk.Access;
-                if (Result)
-                {
-                    Access = eAccess.Success;
-                }
-                else
-                {
-                    Access = eAccess.NotAvailable;
-                }
-            }
-            return Result;
-        }
-
-        /// <summary>
-        /// Метод переносит объект в новое расположение
-        /// </summary>
-        public object_general object_move(Int64 iid_object, Int64 iid_position, Decimal icquantity)
-        {
-            object_general Object_move = null;
-            object_general Object_change = null;
-
-            Int64 id = 0;
-            Int32 error;
-            String desc_error;
-            NpgsqlCommandKey cmdk;
-
-            cmdk = CommandByKey("object_move");
-            if (cmdk != null)
-            {
-                if (!cmdk.Access)
-                {
-                    throw new AccessDataBaseException(404, String.Format(@"Отказано в доступе к методу: {0}!", cmdk.CommandText));
-                }
-            }
-            else
-            {
-                throw new AccessDataBaseException(405, String.Format(@"Не найден метод: {0}!", cmdk.CommandText));
-            }
-
-            cmdk.Parameters["iid_object"].Value = iid_object;
-            cmdk.Parameters["iid_position"].Value = iid_position;
-            cmdk.Parameters["icquantity"].Value = icquantity;
-            cmdk.ExecuteNonQuery();
-
-            id = Convert.ToInt64(cmdk.Parameters["outid"].Value);
-            if (id > 0)
-            {
-                Object_move = object_by_id(id);
-            }
-
-            if (Object_move != null)
-            {
-                //Генерируем событие изменения объекта
-                ObjectChangeEventArgs e = new ObjectChangeEventArgs(Object_move, eAction.Move);
-                ObjectOnChange(e);
-            }
-
-            //Если есть остаток то генерируем изменение остатка
-            if (Object_change != null)
-            {
-                ObjectChangeEventArgs e3 = new ObjectChangeEventArgs(Object_change, eAction.Update);
-                ObjectOnChange(e3);
-            }
-            //Возвращаем сущность
-            return Object_move;
-        }
-
-        /// <summary>
-        /// Метод переносит объект в новое расположение
-        /// </summary>
-        public object_general object_move(object_general Object, position Position, Decimal icuantity)
-        {
-            return object_move(Object.Id, Position.Id, icuantity);
-        }
-
-        //ACCESS
-        /// <summary>
-        /// Проверка прав доступа к методу
-        /// </summary>
-        public Boolean object_move(out eAccess Access)
-        {
-            Boolean Result = false;
-            Access = eAccess.NotFound;
-            NpgsqlCommandKey cmdk;
-
-            cmdk = CommandByKey("object_move");
             if (cmdk != null)
             {
                 Result = cmdk.Access;
